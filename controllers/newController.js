e92215fc00c5b24b99cdf038a038670436e5864a
Nagul75 <nagulk697@gmail.com>
2025-03-02 13:03:15 +0530

    Create newProduct/Category.EJS

--- controllers/newController.js
+++ controllers/newController.js
@@ -1,11 +1,11 @@
 const db = require('../db/queries')
 
 async function createCategoryGet(req, res) {
-    
+    res.render("newCategory", {title: "Create New Category"})
 }
 
 async function createProductGet(req, res) {
-
+    res.render("newProduct", {title: "Create New Product"})
 }
 
 async function createCategoryPost(req , res) {
